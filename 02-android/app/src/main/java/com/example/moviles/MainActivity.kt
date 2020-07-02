3e8de63da30c49293098ea27c19fb9ff33a04463
andrea <as7moc@hotmail.com>
2020-07-02 09:02:22 -0500

    elementos_android_studio_clase10

--- 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.moviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Activity","OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Activity","OnStart")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Activity","OnRestart")
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Activity","OnResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Activity","OnPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Activity","OnStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Activity","OnDestroy")
+
     }
 }
