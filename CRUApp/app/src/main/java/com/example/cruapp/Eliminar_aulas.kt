90c008efc7e8550054e171131496590248149ca0
andrea <as7moc@hotmail.com>
2020-07-31 11:31:41 -0500

    Proyecto_silva_ontaneda

---
+++ CRUApp/app/src/main/java/com/example/cruapp/Eliminar_aulas.kt
@@ -0,0 +1,11 @@
+package com.example.cruapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Eliminar_aulas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eliminar_aulas)
+    }
+}
