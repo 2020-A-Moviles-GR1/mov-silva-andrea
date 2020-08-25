7e0c3b36f6de6c1e0d74c03a88a88e54a2158355
andrea <as7moc@hotmail.com>
2020-08-25 00:28:44 -0500

    calse_peticion_http

---
+++ 02-android/app/src/main/java/com/example/moviles/UsuarioHttp.kt
@@ -0,0 +1,14 @@
+package com.example.moviles
+
+class UsuarioHttp(
+    var id:Int,
+    var createdAt:Long,
+    var updateAt: Long,
+    var cedula: String,
+    var nombre:String,
+    var correo:String,
+    var estadoCivil:String,
+    var password:String
+){
+
+}
